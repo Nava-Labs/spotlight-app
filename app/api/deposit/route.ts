97bd5e1e32a23316926ff02db1f0316dc018bc84
d1ceeth <erwinphanglius10@gmail.com>
2024-09-26 14:13:38 +0700

    feat: simple blinks setup

---
+++ app/api/deposit/route.ts
@@ -0,0 +1,82 @@
+import {
+  ActionGetResponse,
+  ActionPostRequest,
+  ActionPostResponse,
+  ACTIONS_CORS_HEADERS,
+  createPostResponse,
+} from "@solana/actions";
+import { BN } from "bn.js";
+import {
+  clusterApiUrl,
+  ComputeBudgetProgram,
+  Connection,
+  PublicKey,
+  SystemProgram,
+  Transaction,
+} from "@solana/web3.js";
+
+export const GET = (req: Request) => {
+  const payload: ActionGetResponse = {
+    icon: new URL("/megumi.jpg", new URL(req.url).origin).toString(),
+    label: "Pay 0.001 SOL",
+    title: "Spotlight payment",
+    description: "Spotlight payment blinks",
+  };
+
+  return Response.json(payload, {
+    headers: ACTIONS_CORS_HEADERS,
+  });
+};
+
+export const OPTIONS = GET;
+
+export const POST = async (req: Request) => {
+  try {
+    const body: ActionPostRequest = await req.json();
+
+    let account: PublicKey;
+    try {
+      account = new PublicKey(body.account);
+    } catch (e) {
+      return Response.json(
+        { msg: "Invalid Pubkey params!", err: e },
+        { status: 400, headers: ACTIONS_CORS_HEADERS },
+      );
+    }
+
+    const transaction = new Transaction();
+    transaction.add(
+      ComputeBudgetProgram.setComputeUnitPrice({ microLamports: 1000 }),
+      SystemProgram.transfer({
+        fromPubkey: account,
+        toPubkey: new PublicKey("BWm5APyxGkSmDxqKR1KBE1rfBLstai4qopRYeBVMsrZr"),
+        lamports: 1000000, // 0.001 SOL
+      }),
+    );
+
+    transaction.feePayer = account;
+
+    const connection = new Connection(clusterApiUrl("devnet"));
+
+    transaction.recentBlockhash = (
+      await connection.getLatestBlockhash()
+    ).blockhash;
+
+    const payload: ActionPostResponse = await createPostResponse({
+      fields: {
+        transaction,
+        type: "transaction",
+      },
+    });
+
+    return Response.json(payload, {
+      status: 200,
+      headers: ACTIONS_CORS_HEADERS,
+    });
+  } catch (e) {
+    return Response.json(
+      { msg: "Failed to post!", err: e },
+      { status: 400, headers: ACTIONS_CORS_HEADERS },
+    );
+  }
+};
