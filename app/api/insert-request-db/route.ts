adbe760672f1d609175c0f575728796f8a26a3b0
d1ceeth <erwinphanglius10@gmail.com>
2024-10-01 23:26:30 +0700

    fix: unnecessary import

--- app/api/insert-request-db/route.ts
+++ app/api/insert-request-db/route.ts
@@ -1,9 +1,5 @@
 import { getSupabaseServerClient } from "@/lib/supabase/server-client";
-import {
-  ActionPostResponse,
-  ACTIONS_CORS_HEADERS,
-  createPostResponse,
-} from "@solana/actions";
+import { ActionPostResponse, ACTIONS_CORS_HEADERS } from "@solana/actions";
 
 export const POST = async (req: Request) => {
   try {
