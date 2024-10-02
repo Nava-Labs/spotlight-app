556f772e0e4a819c76979cac8d6bcdf798e23a31
xerod <xerod@outlook.com>
2024-10-02 17:00:30 +0700

    db: adjust influencers table

---
+++ supabase/migrations/20241002094709_influencers_table_adjustment.sql
@@ -0,0 +1,12 @@
+alter table "public"."influencers" add column "twitter_id" text;
+
+alter table "public"."influencers" drop column "price";
+alter table "public"."influencers" add column "price" jsonb;
+
+alter table "public"."requests" drop constraint "requests_status_check";
+
+alter table "public"."requests" add constraint "requests_status_check" CHECK ((status = ANY (ARRAY['requested'::text, 'completed'::text, 'approved'::text, 'declined'::text]))) not valid;
+
+alter table "public"."requests" validate constraint "requests_status_check";
+
+
