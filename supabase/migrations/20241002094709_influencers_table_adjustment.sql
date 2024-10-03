29b025ffae78c15371af3ab50867a36d82896e4d
xerod <xerod@outlook.com>
2024-10-03 14:55:16 +0700

    update status constraint

--- supabase/migrations/20241002094709_influencers_table_adjustment.sql
+++ supabase/migrations/20241002094709_influencers_table_adjustment.sql
@@ -5,7 +5,7 @@ alter table "public"."influencers" add column "price" jsonb;
 
 alter table "public"."requests" drop constraint "requests_status_check";
 
-alter table "public"."requests" add constraint "requests_status_check" CHECK ((status = ANY (ARRAY['requested'::text, 'completed'::text, 'approved'::text, 'declined'::text]))) not valid;
+alter table "public"."requests" add constraint "requests_status_check" CHECK ((status = ANY (ARRAY['requested'::text, 'pending'::text, 'approved'::text, 'declined'::text]))) not valid;
 
 alter table "public"."requests" validate constraint "requests_status_check";
 
